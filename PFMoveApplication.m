75e86e57bf8d0a080a4b63403e37472aa6a058bb
Justin <justin@renewedvision.com>
2019-03-22 17:33:35 -0400

    - bumps deployment target to 10.11+

--- PFMoveApplication.m
+++ PFMoveApplication.m
@@ -34,11 +34,6 @@
 #define kStrMoveApplicationQuestionInfoWillRequirePasswd _I10NS(@"Note that this will require an administrator password.")
 #define kStrMoveApplicationQuestionInfoInDownloadsFolder _I10NS(@"This will keep your Downloads folder uncluttered.")
 
-// Needs to be defined for compiling under 10.5 SDK
-#ifndef NSAppKitVersionNumber10_5
-	#define NSAppKitVersionNumber10_5 949
-#endif
-
 // By default, we use a small control/font for the suppression button.
 // If you prefer to use the system default (to match your other alerts),
 // set this to 0.
@@ -109,7 +104,7 @@ void PFMoveToApplicationsFolderIfNecessary(void) {
 	needAuthorization |= ([fm fileExistsAtPath:destinationPath] && ![fm isWritableFileAtPath:destinationPath]);
 
 	// Setup the alert
-	NSAlert *alert = [[[NSAlert alloc] init] autorelease];
+	NSAlert *alert = [[NSAlert alloc] init];
 	{
 		NSString *informativeText = nil;
 
@@ -141,7 +136,7 @@ void PFMoveToApplicationsFolderIfNecessary(void) {
 
 		if (PFUseSmallAlertSuppressCheckbox) {
 			NSCell *cell = [[alert suppressionButton] cell];
-			[cell setControlSize:NSSmallControlSize];
+			[cell setControlSize:NSControlSizeSmall];
 			[cell setFont:[NSFont systemFontOfSize:[NSFont smallSystemFontSize]]];
 		}
 	}
@@ -225,7 +220,7 @@ void PFMoveToApplicationsFolderIfNecessary(void) {
 fail:
 	{
 		// Show failure message
-		alert = [[[NSAlert alloc] init] autorelease];
+		alert = [[NSAlert alloc] init];
 		[alert setMessageText:kStrMoveApplicationCouldNotMove];
 		[alert runModal];
 		MoveInProgress = NO;
@@ -296,27 +291,14 @@ static BOOL IsInDownloadsFolder(NSString *path) {
 
 static BOOL IsApplicationAtPathRunning(NSString *bundlePath) {
 	bundlePath = [bundlePath stringByStandardizingPath];
-
-#if MAC_OS_X_VERSION_MAX_ALLOWED > MAC_OS_X_VERSION_10_5
-	// Use the new API on 10.6 or higher to determine if the app is already running
-	if (floor(NSAppKitVersionNumber) > NSAppKitVersionNumber10_5) {
-		for (NSRunningApplication *runningApplication in [[NSWorkspace sharedWorkspace] runningApplications]) {
-			NSString *runningAppBundlePath = [[[runningApplication bundleURL] path] stringByStandardizingPath];
-			if ([runningAppBundlePath isEqualToString:bundlePath]) {
-				return YES;
-			}
+	
+	for (NSRunningApplication *runningApplication in [[NSWorkspace sharedWorkspace] runningApplications]) {
+		NSString *runningAppBundlePath = [[[runningApplication bundleURL] path] stringByStandardizingPath];
+		if ([runningAppBundlePath isEqualToString:bundlePath]) {
+			return YES;
 		}
-		return NO;
 	}
-#endif
-	// Use the shell to determine if the app is already running on systems 10.5 or lower
-	NSString *script = [NSString stringWithFormat:@"/bin/ps ax -o comm | /usr/bin/grep %@/ | /usr/bin/grep -v grep >/dev/null", ShellQuotedString(bundlePath)];
-	NSTask *task = [NSTask launchedTaskWithLaunchPath:@"/bin/sh" arguments:[NSArray arrayWithObjects:@"-c", script, nil]];
-	[task waitUntilExit];
-
-	// If the task terminated with status 0, it means that the final grep produced 1 or more lines of output.
-	// Which means that the app is already running
-	return [task terminationStatus] == 0;
+	return NO;
 }
 
 static BOOL IsApplicationAtPathNested(NSString *path) {
@@ -336,12 +318,15 @@ static NSString *ContainingDiskImageDevice(NSString *path) {
 	NSString *containingPath = [path stringByDeletingLastPathComponent];
 
 	struct statfs fs;
-	if (statfs([containingPath fileSystemRepresentation], &fs) || (fs.f_flags & MNT_ROOTFS))
+	int err = statfs([containingPath fileSystemRepresentation], &fs);
+	if (err)
 		return nil;
-
+	if (fs.f_flags & MNT_ROOTFS)
+		return nil;
+	
 	NSString *device = [[NSFileManager defaultManager] stringWithFileSystemRepresentation:fs.f_mntfromname length:strlen(fs.f_mntfromname)];
 
-	NSTask *hdiutil = [[[NSTask alloc] init] autorelease];
+	NSTask *hdiutil = [[NSTask alloc] init];
 	[hdiutil setLaunchPath:@"/usr/bin/hdiutil"];
 	[hdiutil setArguments:[NSArray arrayWithObjects:@"info", @"-plist", nil]];
 	[hdiutil setStandardOutput:[NSPipe pipe]];
@@ -349,20 +334,8 @@ static NSString *ContainingDiskImageDevice(NSString *path) {
 	[hdiutil waitUntilExit];
 
 	NSData *data = [[[hdiutil standardOutput] fileHandleForReading] readDataToEndOfFile];
-	NSDictionary *info = nil;
-#if MAC_OS_X_VERSION_MAX_ALLOWED > MAC_OS_X_VERSION_10_5
-	if (floor(NSAppKitVersionNumber) > NSAppKitVersionNumber10_5) {
-		info = [NSPropertyListSerialization propertyListWithData:data options:NSPropertyListImmutable format:NULL error:NULL];
-	}
-	else {
-#endif
-#if MAC_OS_X_VERSION_MIN_REQUIRED < MAC_OS_X_VERSION_10_10
-		info = [NSPropertyListSerialization propertyListFromData:data mutabilityOption:NSPropertyListImmutable format:NULL errorDescription:NULL];
-#endif
-#if MAC_OS_X_VERSION_MAX_ALLOWED > MAC_OS_X_VERSION_10_5
-	}
-#endif
-
+	NSDictionary *info = [NSPropertyListSerialization propertyListWithData:data options:NSPropertyListImmutable format:NULL error:NULL];
+	
 	if (![info isKindOfClass:[NSDictionary class]]) return nil;
 
 	NSArray *images = (NSArray *)[info objectForKey:@"images"];
@@ -390,59 +363,25 @@ static NSString *ContainingDiskImageDevice(NSString *path) {
 
 static BOOL Trash(NSString *path) {
 	BOOL result = NO;
-#if MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_8
-	if (floor(NSAppKitVersionNumber) >= NSAppKitVersionNumber10_8) {
-		result = [[NSFileManager defaultManager] trashItemAtURL:[NSURL fileURLWithPath:path] resultingItemURL:NULL error:NULL];
-	}
-#endif
-#if MAC_OS_X_VERSION_MIN_REQUIRED < MAC_OS_X_VERSION_10_11
-	if (!result) {
-		result = [[NSWorkspace sharedWorkspace] performFileOperation:NSWorkspaceRecycleOperation
-															  source:[path stringByDeletingLastPathComponent]
-														 destination:@""
-															   files:[NSArray arrayWithObject:[path lastPathComponent]]
-																 tag:NULL];
-	}
-#endif
+	NSError *error = nil;
+	result = [[NSFileManager defaultManager] trashItemAtURL:[NSURL fileURLWithPath:path] resultingItemURL:NULL error:&error];
+	if (result)
+		return YES;
 	
-	// As a last resort try trashing with AppleScript.
-	// This allows us to trash the app in macOS Sierra even when the app is running inside
-	// an app translocation image.
-	if (!result) {
-		NSAppleScript *appleScript = [[[NSAppleScript alloc] initWithSource:
-									   [NSString stringWithFormat:@"\
-										set theFile to POSIX file \"%@\" \n\
-									   	tell application \"Finder\" \n\
-									  		move theFile to trash \n\
-									  	end tell", path]] autorelease];
-		NSDictionary *errorDict = nil;
-		NSAppleEventDescriptor *scriptResult = [appleScript executeAndReturnError:&errorDict];
-		if (scriptResult == nil) {
-			NSLog(@"Trash AppleScript error: %@", errorDict);
-		}
-		result = (scriptResult != nil);
-	}
-
-	if (!result) {
-		NSLog(@"ERROR -- Could not trash '%@'", path);
-	}
-
-	return result;
+	NSLog(@"ERROR -- Could not trash '%@'. Error: %@", path, error.localizedDescription);
+	return NO;
 }
 
 static BOOL DeleteOrTrash(NSString *path) {
-	NSError *error;
-
-	if ([[NSFileManager defaultManager] removeItemAtPath:path error:&error]) {
+	NSError *error = nil;
+	BOOL success = [NSFileManager.defaultManager removeItemAtPath:path error:&error];
+	if (success)
 		return YES;
-	}
-	else {
-		// Don't log warning if on Sierra and running inside App Translocation path
-		if ([path rangeOfString:@"/AppTranslocation/"].location == NSNotFound)
-			NSLog(@"WARNING -- Could not delete '%@': %@", path, [error localizedDescription]);
-		
-		return Trash(path);
-	}
+
+	NSLog(@"WARNING -- Could not remove '%@'. Error: %@", path, error.localizedDescription);
+	NSLog(@"INFO -- Will attempt to trash");
+	
+	return Trash(path);
 }
 
 static BOOL AuthorizedInstall(NSString *srcPath, NSString *dstPath, BOOL *canceled) {
@@ -549,13 +488,8 @@ static void Relaunch(NSString *destinationPath) {
 	// OS X >=10.5:
 	// Before we launch the new app, clear xattr:com.apple.quarantine to avoid
 	// duplicate "scary file from the internet" dialog.
-	if (floor(NSAppKitVersionNumber) > NSAppKitVersionNumber10_5) {
-		// Add the -r flag on 10.6
-		preOpenCmd = [NSString stringWithFormat:@"/usr/bin/xattr -d -r com.apple.quarantine %@", quotedDestinationPath];
-	}
-	else {
-		preOpenCmd = [NSString stringWithFormat:@"/usr/bin/xattr -d com.apple.quarantine %@", quotedDestinationPath];
-	}
+	// Add the -r flag on 10.6+
+	preOpenCmd = [NSString stringWithFormat:@"/usr/bin/xattr -d -r com.apple.quarantine %@", quotedDestinationPath];
 
 	NSString *script = [NSString stringWithFormat:@"(while /bin/kill -0 %d >&/dev/null; do /bin/sleep 0.1; done; %@; /usr/bin/open %@) &", pid, preOpenCmd, quotedDestinationPath];
 
